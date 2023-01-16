7db8e6c8a3d45538933c7a876fb0291c59fc815d
wocks1123 <wocks1123@gmail.com>
2023-01-16 11:46:42 +0900

    #1 설정 파일 생성

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    extend: {
+      colors: {
+        'primary': '#2dce89',
+      },
+      spacing: {
+        '120': '30rem',
+        '160': '40rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
