14f86217ff758af491322d0f82256927ce0d0729
wocks1123 <wocks1123@gmail.com>
2023-02-09 10:34:14 +0900

    설정파일 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,17 @@
+const envPath = `config/.env.${process.env.NODE_ENV || 'dev'}`
+require('dotenv').config({ path: envPath })
+
+
 export default {
+  server: {
+    port: 4000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'NSN',
-    htmlAttrs: {
-      lang: 'ko'
-    },
+    title: 'NSN_front',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -18,10 +25,15 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/css/common.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    "@/plugins/scroll-bottom.js",
+    "@/plugins/utils.js",
+    { src: "@/plugins/vClickOutside", ssr: false }
+
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -37,12 +49,60 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+
+    '@nuxtjs/auth',
+
+    'bootstrap-vue/nuxt',
   ],
+
+  bootstrapVue: {
+    components: ['BIcon'],
+    bootstrapCSS:false,
+    icons: true
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: envPath
+  },
+
+  router: {
+    middleware: ['auth']
+  },
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: {
+            url: "/login",
+            method: "post",
+            headers: {
+              accept: 'application/json',
+              // 'Content-Type': 'multipart/form-data'
+            },
+            propertyName: "access_token"
+          },
+          logout: false,
+          user: {
+            url: '/users/me',
+            method: 'get',
+            propertyName: false,
+            autoFetchUser: true
+          },
+        },
+      }
+    }
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
