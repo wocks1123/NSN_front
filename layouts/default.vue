9dd3f2fe459f34b54b530e35d08dbc0fa5552036
wocks1123 <wocks1123@gmail.com>
2023-02-09 10:05:47 +0900

    mobile header 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,11 @@
 <template>
   <!-- Wrapper -->
   <div
-    class="h-screen"
+    class="bg-gray-100"
   >
 
     <!-- Header(mobile only) -->
-    <div
-      class="
-        fixed
-        top-0
-        md:hidden
-        bg-red-300
-        h-14
-        w-full
-        z-20
-      "
-    >
-      Header
-    </div>
+    <MobileHeader />
 
     <!-- Body -->
     <div
@@ -30,14 +18,10 @@
       <!-- Content -->
       <div
         class="
-          pt-14
           md:pt-0
-
           pb-12
           md:pb-0
-
           md:mb-0
-
           md:ml-16
           xl:ml-56
           h-full
