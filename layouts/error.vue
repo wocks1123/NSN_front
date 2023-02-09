384d7e51646cf9353621686d2c2f237fabae839f
wocks1123 <wocks1123@gmail.com>
2023-02-09 09:57:08 +0900

    error page 추가

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 v-if="error.statusCode === 404">Page not found</h2>
+    <h2 v-else>An error occurred</h2>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error",
+  props: ['error']
+}
+</script>
+
+<style scoped>
+
+</style>
